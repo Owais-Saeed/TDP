ba3cd62ae6f12670ebeb68867097002d6ae7b427
brad <167724610+brad99d@users.noreply.github.com>
2024-10-03 17:40:21 +1000

    Adds refinement stages

--- langserve/card_generator.py
+++ langserve/card_generator.py
@@ -16,38 +16,12 @@ from typing import Any, Dict, List
 # Unit: Introduction to Photosynthesis
 # Concept: The Photosynthesis Equation
 
-class CardRequest(BaseModel):
-    level: str
-    topic: str
-    unit: str
-    concept: str
-
 # create the model
 model = ChatGoogleGenerativeAI(
     model="gemini-1.5-flash",
     temperature=0.3,
     max_tokens=5000,
 )
-
-# Create a number of flashcards ranging in difficulty based on a single concept in a unit
-
-# ---- [ STEP 1 ] -- [ Create Flashcards ] ----
-
-str_parser = StrOutputParser()
-
-flashcards_prompt = PromptTemplate(
-    template = r"""
-You will create a set of 8-10 high-quality **concise** educational flashcards for a {level} level student. The course topic is {topic}, and this set is part of {unit}. Flashcards must focus on **active recall** by framing the front of the card as a **question** and providing a clear, concise answer on the back.
-
-The flashcards should cover the following key concept:
-{concept}
-\n""",
-    input_variables = ["level", "topic", "unit", "concept"],
-)
-
-flashcards_chain = flashcards_prompt | model
-
-# ---- [ STEP 2 ] -- [ Output valid format ] ----
 
 # define the data structure using pydantic
 class Card(BaseModel):
@@ -57,7 +31,47 @@ class Card(BaseModel):
 class Set(BaseModel):
     set: List[Card] = Field(description="A collection of flashcards")
 
+# create the parsers
+str_parser = StrOutputParser()
 json_parser = JsonOutputParser(pydantic_object=Set)
+
+# ---- [ STEP 1 ] -- [ Create Flashcards ] ----
+
+flashcards_prompt = PromptTemplate(
+    template = r"""
+You will create a set of 8-10 high-quality **concise** educational flashcards for a {level} level student. The course topic is {topic}, and this set is part of {unit}. Flashcards must focus on **active recall** by framing the front of the card as a **question** and providing a clear, concise answer on the back.
+
+The flashcards will range between beginner, intermediate, and advanced levels.
+
+Use LaTeX to represent **all** mathematical expressions, equations, and formulas.
+
+The flashcards should cover the following key concept:
+{concept}
+\n""",
+    input_variables = ["level", "topic", "unit", "concept"],
+)
+
+flashcards_chain = flashcards_prompt | model
+
+# ---- [ STEP 2 ] -- [ Refine the cards ] ----
+
+refinement_prompt = PromptTemplate(
+    template = r"""
+You must edit the following flashcards to use concise language.
+
+All cards must span a single line, new lines are not allowed.
+
+All cards must retain a front (prompt) and back (response) format.
+
+## The Flashcards
+{flashcards}
+\n""",
+    input_variables = ["flashcards"],
+)
+
+refinement_chain = refinement_prompt | model
+
+# ---- [ STEP 3 ] -- [ Output valid format ] ----
 
 formatting_prompt = PromptTemplate(
     template = r"""
@@ -88,4 +102,4 @@ formatting_chain = formatting_prompt | model | json_parser
 
 # ------ [ Final Chain ] ------
 
-final_chain = flashcards_chain | formatting_chain
+final_chain = flashcards_chain | refinement_chain | formatting_chain
