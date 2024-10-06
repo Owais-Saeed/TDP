45ac7619e84c2ca75de70ca13d87f34e2c72618f
brad <167724610+brad99d@users.noreply.github.com>
2024-10-07 01:58:13 +1100

    Adds modular navigation bar

--- app/blueprints/dashboard/routes.py
+++ app/blueprints/dashboard/routes.py
@@ -4,7 +4,17 @@ from flask import render_template
 from . import dashboard_bp
 from flask_login import login_required, current_user
 
+options = [
+    { 'name': 'Profile', 'url': '#', 'icon': '' },
+    { 'name': 'Settings', 'url': '#', 'icon': '' },
+    { 'name': 'Sign Out', 'url': '/auth/logout', 'icon': '' },
+]
+
 @dashboard_bp.route('/', methods=['GET'])
 @login_required
 def home():
-    return render_template('dashboard/home.html', user=current_user)
+    return render_template(
+        'dashboard/home.html',
+        user=current_user,
+        options_menu=options,
+    )
--- app/blueprints/main/routes.py
+++ app/blueprints/main/routes.py
@@ -4,10 +4,12 @@ from flask_login import current_user
 
 @main_bp.route('/')
 def home():
+    '''
     flash('Welcome.', 'success')
     flash('Welcome.', 'danger')
     flash('Welcome.', 'warning')
     flash('Welcome.', 'info')
+    '''
     # redirect user to dashboard if already logged in
     if current_user.is_authenticated:
         return redirect(url_for('dashboard.home'))
